6a1ed83747dc65b75b85f0c1b29068ad9a12db25
Stephan Kolontay <stepan.kolontaj@gmail.com>
2023-05-12 23:04:32 +0300

    update char_sort

--- Topics/Arrays&Hashing/valid_anagram/char_sort.rs
+++ Topics/Arrays&Hashing/valid_anagram/char_sort.rs
@@ -1,3 +1,6 @@
+// @Runtime 12 ms (7.31%)
+// @Memory 2.5 MB (12.58%)
+
 pub fn is_anagram(s: String, t: String) -> bool {
     let mut sch: Vec<char> = s.chars().collect();
     let mut tch: Vec<char> = t.chars().collect();
