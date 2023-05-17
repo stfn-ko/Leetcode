87153748a88c3d81bdb387527452de52df5b8795
stfn_ko <stepan.kolontaj@gmail.com>
2023-05-17 20:34:10 +0100

    added solution

--- Topics/Arrays&Hashing/product_of_array_except_self/sln.cpp
+++ Topics/Arrays&Hashing/product_of_array_except_self/sln.cpp
@@ -1,4 +1,15 @@
-std::vector<int> productExceptSelf(std::vector<int> &nums)
-{
+// @Runtime: 15 ms (98.91%)
+// @Memory: 23.9 MB (86.61%)
 
+std::vector<int> productExceptSelf(std::vector<int> &input)
+{
+    std::vector<int> answer(input.size(), 1);
+
+    for (int i = 0; i < input.size() - 1; ++i)
+        answer[i + 1] = input[i] * answer[i];
+
+    for (int i = input.size() - 1, product = input[i]; i > 0; --i, product *= input[i])
+        answer[i - 1] *= product;
+
+    return answer;
 }
