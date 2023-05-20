5b5319af3bf08e430abb7f6065c5bbfaebfdbec2
stfn_ko <stepan.kolontaj@gmail.com>
2023-05-20 22:00:03 +0100

    added c++ solution for top k

--- Topics/Arrays&Hashing/top_k_frequent_elements/sln.cpp
+++ Topics/Arrays&Hashing/top_k_frequent_elements/sln.cpp
@@ -1,3 +1,20 @@
 vector<int> topKFrequent(vector<int> &nums, int k)
 {
+    priority_queue<pair<int, int>> pq;
+    unordered_map<int, int> um;
+    vector<int> v;
+    v.reserve(k);
+
+    for (const auto &num : nums)
+        um[num]++;
+    for (const auto &m : um)
+    {
+        pq.push(make_pair(m.second, m.first));
+        if (pq.size() > um.size() - k)
+        {
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+    }
+    return v;
 }
