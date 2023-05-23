8776ed722a4bfad0f3f97479fe02c643a68539fe
Stephan Kolontay <stepan.kolontaj@gmail.com>
2023-05-23 22:54:05 +0100

    valid sudoku

--- Topics/Arrays&Hashing/valid_sudoku/sln.cpp
+++ Topics/Arrays&Hashing/valid_sudoku/sln.cpp
@@ -8,11 +8,11 @@ bool isValidSudoku(vector<vector<char>>& board) {
 
   for (int i = 0; i < 9; ++i) {
     for (int j = 0; j < 9; ++j) {
-      if(board[i][j] != '.') {
-        int n = board[i][j]-'1';
-        if(r[i][n]++) return 0;
-        if(c[j][n]++) return 0;
-        if(b[i/3][j/3][n]++) return 0;
+        int w = board[i][j]-0b110000;
+        if (w < 0) break;
+        if(r[i][w]++) return 0;
+        if(c[j][w]++) return 0;
+        if(b[i/3][j/3][w]++) return 0;
       }
     }
   }
